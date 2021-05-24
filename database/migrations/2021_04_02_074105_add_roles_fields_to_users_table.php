90946915982fc27f42e8adfc20f3734c3c39e1ce
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-05-24 08:54:03 +0200

    fix migrations

--- database/migrations/2021_04_02_074105_add_roles_fields_to_users_table.php
+++ database/migrations/2021_04_02_074105_add_roles_fields_to_users_table.php
@@ -14,6 +14,7 @@ class AddRolesFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained();
         });
     }
 
